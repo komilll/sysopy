4a2e3329a8ca32b560338fd3cefe17694c0e02f3
komilll <kamil9772@wp.pl>
2019-12-22 15:18:48 +0100

    cw06 con

--- cw06/zad1/barber.c
+++ cw06/zad1/barber.c
@@ -3,8 +3,16 @@
 int semID = -1;
 int memID = -1;
 
+void shave()
+{
+    struct timespec monotime;
+    clock_gettime(CLOCK_MONOTONIC, &monotime);   
+    printf("%ld.%ld Barber: start shaving client %d\n", monotime.tv_sec, monotime.tv_nsec, data->clientPIDs[0]);
+}
+
 void cleanUpAtExit()
 {
+    //Clean up at exit
     if (semID != -1){
         semctl(semID, 0, IPC_RMID);
     }
@@ -15,6 +23,7 @@ void cleanUpAtExit()
 
 void initializeBarber()
 {
+    //Initialize semaphore
     key_t key = ftok(PROPERTIES_PATH, PROPERTIES_PROGRAM_GENERATOR_ID);
 
     semID = semget(key, 1, IPC_CREAT | IPC_EXCL);
@@ -24,6 +33,7 @@ void initializeBarber()
     }
     semctl(semID, 0, SETVAL, 0);
 
+    //Initialize shared memory
     memID = shmget(key, sizeof(struct sharedData), IPC_CREAT | IPC_EXCL | 0666);
     if (memID == -1){
         printf("Failed to create shared memory\n");
@@ -40,11 +50,12 @@ void initializeBarber()
 
 int main(int argc, char* argv[])
 {
+    //Prepare signals to stop execution
     if (signal(SIGINT, cleanUpAtExit) == SIG_ERR){
+        //--Practial purpose - it's easier to test with Ctrl+C
         printf("Failed to assign SIGINT function in client\n");
         return -1;
     }
-
     if (signal(SIGTERM, cleanUpAtExit) == SIG_ERR){
         printf("Failed to assign SIGTERM function in client\n");
         return -1;
@@ -56,48 +67,54 @@ int main(int argc, char* argv[])
         return -1;
     }
 
+    //Initialize sempahore and shared memory
     initializeBarber();
 
+    //Set value of semaphore to 1
     giveSemaphore(semID);
+
+    //Check data and initialize barber state
     data = shmat(memID, NULL, 0);
     if (data == NULL){
         printf("Failed to get data\n");
         return -1;
     }
     data->curBarberState = CHECK_QUEUE;
-
-    struct sysinfo info;
-    sysinfo(&info);
+        
+    struct timespec monotime;
 
     while (1){
         takeSemaphore(semID);
 
-        struct timespec monotime;
+        //Get current time
         clock_gettime(CLOCK_MONOTONIC, &monotime);   
         
         switch (data->curBarberState){
             case CHECK_QUEUE:
                 printf("%ld.%ld Barber: client count - %d\n", monotime.tv_sec, monotime.tv_nsec, data->clientsLeft);
 
+                //Go to sleep - no clients
                 if (data->clientsLeft == 0){
-                    //Go to sleep
                     data->curBarberState = SLEEPING;
                     printf("%ld.%ld Barber: go to sleep\n", monotime.tv_sec, monotime.tv_nsec);
-                } else {
-                    //Wait until client sit on chair
+                }
+                //Wait until client sit on chair - (clientsLeft > 0 )
+                else {                    
                     data->curBarberState = WAITING_TO_SIT;
                     printf("%ld.%ld Barber: invite client %d to sit\n", monotime.tv_sec, monotime.tv_nsec, data->clientPIDs[0]);
                 }
                 break;
 
             case WAKING_UP:
+                //Client is trying to wake up barber
                 printf("%ld.%ld Barber: waking up\n", monotime.tv_sec, monotime.tv_nsec);
                 data->curBarberState = SHAVING;
                 break;
 
             case SHAVING:
-                printf("%ld.%ld Barber: start shaving client %d\n", monotime.tv_sec, monotime.tv_nsec, data->clientPIDs[0]);
-                //Call Shaving() function
+                //Do actual shaving and set FINISHED_SHAVING to inform client about it
+                shave(); //Used only to demonstrate time difference between start-end of shaving
+                clock_gettime(CLOCK_MONOTONIC, &monotime);   
                 printf("%ld.%ld Barber: end shaving client %d\n", monotime.tv_sec, monotime.tv_nsec, data->clientPIDs[0]);
                 data->curBarberState = FINISHED_SHAVING;
                 break;
@@ -105,6 +122,7 @@ int main(int argc, char* argv[])
             case WAITING_TO_SIT:
             case SLEEPING:
             case FINISHED_SHAVING:
+            //States which are waiting for client action
             default:
                 break;
         }
--- cw06/zad1/clientHandler.c
+++ cw06/zad1/clientHandler.c
@@ -8,82 +8,113 @@ enum clientState state;
 
 void putClientToQueue()
 {
-    if (desiredActions > 0 && state == IDLE){
-        state = WAIT_IN_QUEUE;
-        struct sysinfo info;
-        sysinfo(&info);
+    //If client has entered barber shop - put him in queue
+    if (state == IDLE && desiredActions > 0){
         struct timespec monotime;
         clock_gettime(CLOCK_MONOTONIC, &monotime);
-        printf("%ld.%ld Client: sit in queue ~ %d\n", monotime.tv_sec, monotime.tv_nsec, getpid());
+
+        for (int i = 0; i < data->clientsLeft; i++){
+            if (data->clientPIDs[i] == getpid()){
+                printf("%ld.%ld Client: sit in queue ~ %d\n", monotime.tv_sec, monotime.tv_nsec, getpid());
+                state = WAIT_IN_QUEUE;
+                return;
+            }
+        }       
+
+        desiredActions = 0; 
+        printf("%ld.%ld Client: leave shop due to full queue ~ %d\n", monotime.tv_sec, monotime.tv_nsec, getpid());
+        exit(0);
     }
 }
 
 void createClient(int actions)
 {
+    //Set how many shaves client want
     desiredActions = actions;
     if (desiredActions == 0){
         return;
     }
 
+    //Create client process
     currentChildPID = fork();
     if (currentChildPID < 0){
         printf("Failed to create client\n");
         exit(-1);
     } else if (currentChildPID == 0){
+        //Initialize client state - don't rely on zero-initializiation
         state = IDLE;
     } else{
+        //Add client data to queued
         data = shmat(memID, NULL, 0);
         if (data->clientsLeft == MAX_CLIENT_COUNT){
-            printf("Reached maximum number of clients to create\n");
-            exit(-1);
+            return;
+        }
+        if (currentChildPID == getpid()){
+            return;
         }
         data->clientPIDs[data->clientsLeft] = currentChildPID;
+        printf("Child id: %d\n", currentChildPID);
         data->clientsLeft++;
     }
 }
 
 int main(int argc, char* argv[])
 {
+    //Get semaphore and shared memory
     key_t key = ftok(PROPERTIES_PATH, PROPERTIES_PROGRAM_GENERATOR_ID);
     memID = shmget(key, sizeof(struct sharedData), 0);
     if (memID == -1){
         printf("Failed to create shared memory\n");
         exit(-1);
     }
-
-    srand(getpid());
-    for (int i = 0; i < 10; i++) {
-        if (currentChildPID != 0){
-            createClient(1 + rand() % 5);
-            // createClient(2);
-        }
-    }
     data = shmat(memID, NULL, 0);
+    int clientsCount = 10;
 
-    struct sysinfo info;
-    sysinfo(&info);
+    if (argc == 3){
+        clientsCount = atoi(argv[1]);   
+        int shaveCount = atoi(argv[2]);
+        //Create clients with same shave number
+        for (int i = 0; i < clientsCount; i++) {
+            createClient(shaveCount);
+        }
+    } else if (argc == 2 || argc == 1){
+        if (argc == 2){
+            clientsCount = atoi(argv[1]);
+        }
+        //Create clients with random shave number
+        srand(getpid());
+        for (int i = 0; i < clientsCount; i++) {
+            if (currentChildPID != 0){
+                createClient(1 + rand() % 5);
+            }
+        }
+    } 
+
+    struct timespec monotime;
     int continueLoop = 1;
 
     while(continueLoop){
         if (currentChildPID == 0){
             takeSemaphore(data->semID);
-
-            struct timespec monotime;
             clock_gettime(CLOCK_MONOTONIC, &monotime);
 
             switch (data->curBarberState){
                 case WAITING_TO_SIT:
+                    //Barber has invited client to sit - check if client is the invited one
                     if (state == WAIT_IN_QUEUE && getpid() == data->clientPIDs[0]){
                         state = SIT_ON_CHAIR;
                         printf("%ld.%ld Client: sit on chair ~ %d with %d shaves left\n", monotime.tv_sec, monotime.tv_nsec, getpid(), desiredActions - performedActions);
                         data->curBarberState = SHAVING;
-                    } else {
+                    } 
+                    //Put him in queue if standing and not the invited one
+                    else {
                         putClientToQueue();
                     }
                     break;
                 case FINISHED_SHAVING:        
                     //Perform action only on client which is sitting on chair         
                     if (state == SIT_ON_CHAIR){
+                        //Do shave and check if all shaves already done
                         performedActions++;
                         if (desiredActions == performedActions){
                             printf("%ld.%ld Client: leave barber ~ %d\n", monotime.tv_sec, monotime.tv_nsec, getpid());
@@ -91,7 +122,7 @@ int main(int argc, char* argv[])
                             continueLoop = 0;
                             state = IDLE;
                         } else {
-                            //if (sitsLeft == 0) -> printf("leave barber due to full queue")                    
+                            //Wait for next shave in queue
                             printf("%ld.%ld Client: sit in queue ~ %d\n", monotime.tv_sec, monotime.tv_nsec, getpid());
                             goAtQueueEnd(memID);
                             state = WAIT_IN_QUEUE;
@@ -108,11 +139,8 @@ int main(int argc, char* argv[])
                         data->curBarberState = WAKING_UP;
                         moveToQueueBeginning(memID, getpid());
                         printf("%ld.%ld Client: wake up barber ~ %d\n", monotime.tv_sec, monotime.tv_nsec, getpid());
+                        clock_gettime(CLOCK_MONOTONIC, &monotime);
                         printf("%ld.%ld Client: sit on chair ~ %d with %d shaves left\n", monotime.tv_sec, monotime.tv_nsec, getpid(), desiredActions - performedActions);
-
-                        // for (int i = 0; i < data->clientsLeft; i++){
-                        //     printf("Client: %d\n", data->clientPIDs[i]);  
-                        // } 
                     }
                     break;
 
@@ -124,6 +152,7 @@ int main(int argc, char* argv[])
             }
             giveSemaphore(data->semID);
         } else if (data->clientsLeft == 0){
+            //No clients in barber shop - close handler process
             printf("\n ~ No clients left - end clientHandler process ~ \n");
             return 0;
         }
