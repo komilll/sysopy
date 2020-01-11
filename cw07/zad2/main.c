a48f20975321c29c0d60cc54b04c8167c8e9e4b6
komilll <kamil9772@wp.pl>
2020-01-11 21:33:56 +0100

    File structure cw06

---
+++ cw07/zad2/main.c
@@ -0,0 +1,14 @@
+#define _XOPEN_SOURCE 500
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/sysinfo.h>
+#include <time.h>
+
+
+int main(int argc, char* argv[])
+{
+    
+    return 0;
+}
