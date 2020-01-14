feff4d6f33979d74e82cb85a13bb6d6fa0344c68
komilll <kamil9772@wp.pl>
2020-01-14 08:56:37 +0100

    cw07/zad1 ready

--- cw07/zad1/main.c
+++ cw07/zad1/main.c
@@ -43,6 +43,16 @@ int isWorking = 1;
 
 //////////////////////
 
+void sigHandler(int signo)
+{
+    if (signo != SIGINT && signo != SIGALRM){
+        return;
+    }
+    printf("Correctly interrupted program execution\n");
+    exit(0);
+}
+
+//Check if passed line has any chars (apart from EOF and '\0\)
 int isEmptyLine(char* line)
 {
     if (strlen(line) == 0){
@@ -53,11 +63,23 @@ int isEmptyLine(char* line)
             return 1;
         }
         if (line[i] != ' '){
-            // printf("Line #%d: %d\n", i, line[i]);
             return 0;
         }
     }
     return 1;
+}
+
+int compareLength(char* line)
+{
+    if (searchType == 0){
+        return (strlen(line) < L);
+    } else if (searchType == 1){
+        return (strlen(line) < L);
+    } else if (searchType == 2){
+        return (strlen(line) == L);
+    }
+    printf("Unknown search type passed - terminate program\n");
+    exit(-1);
 }
 
 void readConfigurationFile(char* filename)
@@ -100,60 +122,108 @@ void initialize()
     //Intialize threads
     p_threads = malloc((size_t) P * sizeof(pthread_t));
     c_threads = malloc((size_t) K * sizeof(pthread_t));
+
+    //Prepare signal
+    if (signal(SIGINT, sigHandler) == SIG_ERR){
+        printf("Failed to assign SIGINT function in client\n");
+        exit(-1);
+    }
+    //Close after specified time
+    if (nk > 0){
+        if (signal(SIGALRM, sigHandler) == SIG_ERR){
+            printf("Failed to assign SIGINT function in client\n");
+            exit(-1);
+        }
+    }
 }
 
 void* producerFunc()
 {
+    //Prepare index and char array
     int currentIndex = 0;
     char line[MAX_LINE_LENGTH];
+    struct timespec monotime;
+
+    //Continue execution if any line in the file wasn't read
     while (fgets(line, MAX_LINE_LENGTH, mainFile)){
+        if (debug) printf("Producer #%ld gets line\n", pthread_self());
+
+        //Lock mutex at beginning
         pthread_mutex_lock(p_mutex);
 
+        //Wait to free ring buffer at producer index
         while (buffer[p_index] != NULL)
             pthread_cond_wait(&w_cond, p_mutex);
 
+        //Lock buffer index
         currentIndex = p_index;
         pthread_mutex_lock(&b_mutex[currentIndex]);
 
+        clock_gettime(CLOCK_MONOTONIC, &monotime);  
+        if (debug) printf("Producer #%ld takes buffer index: %d - time: %ld.%ld\n", pthread_self(), p_index, monotime.tv_sec, monotime.tv_nsec);
+
+        //Pass data to buffer
         buffer[currentIndex] = malloc((strlen(line) + 1) * sizeof(char));
         strcpy(buffer[currentIndex], line);
         p_index = (p_index + 1) % N;        
+
+        if (debug) printf("Producer #%ld saved line to buffer index: %d\n", pthread_self(), p_index);
         
+        //Unlock buffer; Broadcast info to readers; Unlock producer
         pthread_mutex_unlock(&b_mutex[currentIndex]);
         pthread_cond_broadcast(&r_cond);
         pthread_mutex_unlock(p_mutex);
+        if (debug) printf("Producer #%ld unlocks mutexes and broadcast to cond to consumers\n", pthread_self());
     }
-
+    if (debug) printf("Producer #%ld finished his work - terminate\n", pthread_self());
     return NULL;
 }
 
 void* consumerFunc()
 {
+    //Prepare index and line string pointer
     int currentIndex = 0;
     char *line;
-    while (1){  
-        pthread_mutex_lock(c_mutex);
+    struct timespec monotime;
 
+    while (1){  
+        //Lock mutex at beginning
+        pthread_mutex_lock(c_mutex);
+        if (debug) printf("~~Consumer #%ld try to take c_mutex\n", pthread_self());
+
+        //Wait to fill ring buffer at consumer index
         while(buffer[c_index] == NULL){
+            //If there is signal to stop - unlock mutex and finish thread
             if (!isWorking){
+                if (debug) printf("~~Consumer #%ld finished work\n", pthread_self());
                 pthread_mutex_unlock(c_mutex);
                 return NULL;
             }
             pthread_cond_wait(&r_cond, c_mutex);
-        }
+        }        
 
+        //Lock buffer index
         currentIndex = c_index;
-        pthread_mutex_lock(&b_mutex[currentIndex]);
+        pthread_mutex_lock(&b_mutex[currentIndex]);        
+        
+        clock_gettime(CLOCK_MONOTONIC, &monotime);
+        if (debug) printf("~~Consumer #%ld found not-empty buffer index: %d - time: %ld.%ld\n", pthread_self(), c_index, monotime.tv_sec, monotime.tv_nsec);
+
+        //Read line, compare, cleanup
         line = buffer[currentIndex];
-        if (!isEmptyLine(line) && strlen(line) < L){
+        if (!isEmptyLine(line) && compareLength(line)){
+            if (debug) printf("~~Consumer #%ld found correct line\n", pthread_self());
             printf("%s\n", line);
         }
         buffer[currentIndex] = NULL;
         free(line);
-        c_index = (c_index + 1) % N;
+        c_index = (c_index + 1) % N;        
+
+        //Unlock buffer; Broadcast info to writers; Unlock consumer
         pthread_mutex_unlock(&b_mutex[currentIndex]);
         pthread_cond_broadcast(&w_cond);
         pthread_mutex_unlock(c_mutex);
+        if (debug) printf("~~Consumer #%ld unlocks its mutexes and broadcast to writers\n", pthread_self());
         usleep(10);
     }
 
@@ -168,6 +238,11 @@ void runThreads()
     for (int i = 0; i < K; ++i){
         pthread_create(&c_threads[i], NULL, consumerFunc, NULL);
     }
+    //Stop execution after specified time
+    if (nk > 0){
+        ualarm(nk, 0);
+        // alarm(nk);
+    }
 }
 
 void joinThreads()
@@ -175,6 +250,8 @@ void joinThreads()
     for (int i = 0; i < P; ++i){
         pthread_join(p_threads[i], NULL);
     }
+    //When producers finished working 
+    //- indicate that consumers after finding empty buffer should just close themselves
     isWorking = 0;
     pthread_cond_broadcast(&r_cond);
     for (int i = 0; i < K; ++i){
@@ -184,7 +261,6 @@ void joinThreads()
 
 void cleanup()
 {
-    return;
     if (mainFile){
         fclose(mainFile);
     }
@@ -193,7 +269,6 @@ void cleanup()
         if (buffer[i]){
             free(buffer[i]);
         }
-        free(buffer);
     }
 
     for (int i = 0; i < N; ++i){
@@ -201,9 +276,9 @@ void cleanup()
     }
     free(b_mutex);
     pthread_mutex_destroy(c_mutex);
-    // free(c_mutex);
+    free(c_mutex);
     pthread_mutex_destroy(p_mutex);
-    // free(p_mutex);
+    free(p_mutex);
 
     pthread_cond_destroy(&r_cond);
     pthread_cond_destroy(&w_cond);
